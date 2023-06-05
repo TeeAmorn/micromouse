fdf3db782241108dbdeb58362d48d49d7c7d12ff
TeeAmorn <theerut.amorn@gmail.com>
2023-06-05 00:20:14 -0500

    Added logic to scan all types of walls

--- src/maze.rs
+++ src/maze.rs
@@ -7,7 +7,7 @@ pub struct Maze {}
 
 impl Maze {
     pub fn build(width: usize, height: usize, _config: WallWeights) -> Option<Maze> {
-        if width < 1 || height < 1 {
+        if width < 2 || height < 2 {
             return None;
         }
 
@@ -27,6 +27,8 @@ impl Maze {
         Some(Maze {})
     }
 }
+
+// ========== Edge and Cell Coordinates-ID Conversion ==========
 
 const fn get_edge_coord(width: usize, height: usize, mut id: usize) -> Option<(usize, usize)> {
     let mut row = 2 * (id / (width * 2 - 1));
@@ -86,6 +88,8 @@ const fn get_cell_id(width: usize, height: usize, row: usize, col: usize) -> Opt
     Some(row / 2 * width + col / 2)
 }
 
+// ========== Wall Weights ==========
+
 pub struct WallWeights {
     // Name Format: type[0][1][2][3]_[4][5][6]
     //     0   3
@@ -97,111 +101,601 @@ pub struct WallWeights {
     //   |   |
     //  -  ~  -
     //   |   |
-    pub type_111_111: u32,
+    pub type_111x111: u32,
+
+    //   |
+    //  -  ~  -
+    //   |   |
+    pub type_111x011: u32,
 
     //   |   |
     //  -  ~
     //   |   |
-    pub type_111_101: u32,
+    pub type_111x101: u32,
 
     //   |   |
     //  -  ~
     //   |
-    pub type_111_100: u32,
+    pub type_111x100: u32,
 
     //   |
     //  -  ~  -
     //   |
-    pub type_111_010: u32,
+    pub type_111x010: u32,
 
     //   |
     //  -  ~
     //   |
-    pub type_111_000: u32,
+    pub type_111x000: u32,
 
     //   |   |
     //     ~
     //   |   |
-    pub type_101_101: u32,
+    pub type_101x101: u32,
 
     //   |
     //     ~  -
     //   |   |
-    pub type_101_011: u32,
+    pub type_101x011: u32,
 
     //   |
     //     ~  -
     //   |
-    pub type_101_010: u32,
+    pub type_101x010: u32,
 
     //   |
     //     ~
     //   |   |
-    pub type_101_001: u32,
+    pub type_101x001: u32,
 
     //   |
     //     ~
     //   |
-    pub type_101_000: u32,
+    pub type_101x000: u32,
 
     //
     //  -  ~  -
     //   |   |
-    pub type_011_011: u32,
+    pub type_011x011: u32,
 
     //       |
     //  -  ~  -
     //   |
-    pub type_011_110: u32,
+    pub type_011x110: u32,
 
     //
     //  -  ~  -
     //   |
-    pub type_011_010: u32,
+    pub type_011x010: u32,
 
     //
     //  -  ~
     //   |   |
-    pub type_011_001: u32,
+    pub type_011x001: u32,
 
     //       |
     //  -  ~
     //   |
-    pub type_011_100: u32,
+    pub type_011x100: u32,
 
     //
     //  -  ~
     //   |
-    pub type_011_000: u32,
+    pub type_011x000: u32,
 
     //
     //  -  ~  -
     //
-    pub type_010_010: u32,
+    pub type_010x010: u32,
 
     //       |
     //  -  ~
     //
-    pub type_010_100: u32,
+    pub type_010x100: u32,
 
     //
     //  -  ~
     //
-    pub type_010_000: u32,
+    pub type_010x000: u32,
 
     //
     //     ~
     //   |   |
-    pub type_001_001: u32,
+    pub type_001x001: u32,
 
     //       |
     //     ~
     //   |
-    pub type_001_100: u32,
+    pub type_001x100: u32,
 
     //
     //     ~
     //   |
-    pub type_001_000: u32,
+    pub type_001x000: u32,
 }
 
+enum WallType {
+    Type111x111,
+    Type111x011,
+    Type111x101,
+    Type111x100,
+    Type111x010,
+    Type111x000,
+    Type101x101,
+    Type101x011,
+    Type101x010,
+    Type101x001,
+    Type101x000,
+    Type011x011,
+    Type011x110,
+    Type011x010,
+    Type011x001,
+    Type011x100,
+    Type011x000,
+    Type010x010,
+    Type010x100,
+    Type010x000,
+    Type001x001,
+    Type001x100,
+    Type001x000,
+}
+
+type NeighborsOneSided = (usize, usize, usize);
+type NeighborsTwoSided = (usize, usize, usize, usize, usize, usize);
+
+fn get_wall_type(width: usize, height: usize, edges: &Vec<bool>, id: usize) -> Option<WallType> {
+    let row;
+    let col;
+
+    if let Some((i, j)) = get_edge_coord(width, height, id) {
+        row = i;
+        col = j;
+    } else {
+        return None;
+    }
+
+    None
+}
+
+fn contains_wall_type_111x111(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 111x111
+    edges[neighbors.0]
+        && edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && edges[neighbors.4]
+        && edges[neighbors.5]
+}
+
+fn contains_wall_type_111x011(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 111x011
+    let v1 = edges[neighbors.0]
+        && edges[neighbors.1]
+        && edges[neighbors.2]
+        && !edges[neighbors.3]
+        && edges[neighbors.4]
+        && edges[neighbors.5];
+    // 111x110
+    let v2 = edges[neighbors.0]
+        && edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && edges[neighbors.4]
+        && !edges[neighbors.5];
+    // 011x111
+    let v3 = !edges[neighbors.0]
+        && edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && edges[neighbors.4]
+        && edges[neighbors.5];
+    // 110x111
+    let v4 = edges[neighbors.0]
+        && edges[neighbors.1]
+        && !edges[neighbors.2]
+        && edges[neighbors.3]
+        && edges[neighbors.4]
+        && edges[neighbors.5];
+    v1 || v2 || v3 || v4
+}
+
+fn contains_wall_type_111x101(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 111x101
+    let v1 = edges[neighbors.0]
+        && edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && !edges[neighbors.4]
+        && edges[neighbors.5];
+    // 101x111
+    let v2 = edges[neighbors.0]
+        && !edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && edges[neighbors.4]
+        && edges[neighbors.5];
+    v1 || v2
+}
+
+fn contains_wall_type_111x100(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 111x100
+    let v1 = edges[neighbors.0]
+        && edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && !edges[neighbors.4]
+        && !edges[neighbors.5];
+    // 111x001
+    let v2 = edges[neighbors.0]
+        && edges[neighbors.1]
+        && edges[neighbors.2]
+        && !edges[neighbors.3]
+        && !edges[neighbors.4]
+        && edges[neighbors.5];
+    // 100x111
+    let v3 = edges[neighbors.0]
+        && !edges[neighbors.1]
+        && !edges[neighbors.2]
+        && edges[neighbors.3]
+        && edges[neighbors.4]
+        && edges[neighbors.5];
+    // 001x111
+    let v4 = !edges[neighbors.0]
+        && !edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && edges[neighbors.4]
+        && edges[neighbors.5];
+    v1 || v2 || v3 || v4
+}
+
+fn contains_wall_type_111x010(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 111x010
+    let v1 = edges[neighbors.0]
+        && edges[neighbors.1]
+        && edges[neighbors.2]
+        && !edges[neighbors.3]
+        && edges[neighbors.4]
+        && !edges[neighbors.5];
+    // 010x111
+    let v2 = !edges[neighbors.0]
+        && edges[neighbors.1]
+        && !edges[neighbors.2]
+        && edges[neighbors.3]
+        && edges[neighbors.4]
+        && edges[neighbors.5];
+    v1 || v2
+}
+
+fn contains_wall_type_111x000(edges: &Vec<bool>, neighbors: NeighborsOneSided) -> bool {
+    // 111x000 and 000x111
+    edges[neighbors.0] && edges[neighbors.1] && edges[neighbors.2]
+}
+
+fn contains_wall_type_101x101(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 101x101
+    edges[neighbors.0]
+        && !edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && !edges[neighbors.4]
+        && edges[neighbors.5]
+}
+
+fn contains_wall_type_101x011(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 101x011
+    let v1 = edges[neighbors.0]
+        && !edges[neighbors.1]
+        && edges[neighbors.2]
+        && !edges[neighbors.3]
+        && edges[neighbors.4]
+        && edges[neighbors.5];
+    // 101x110
+    let v2 = edges[neighbors.0]
+        && !edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && edges[neighbors.4]
+        && !edges[neighbors.5];
+    // 011x101
+    let v3 = !edges[neighbors.0]
+        && edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && !edges[neighbors.4]
+        && edges[neighbors.5];
+    // 110x101
+    let v4 = edges[neighbors.0]
+        && edges[neighbors.1]
+        && !edges[neighbors.2]
+        && edges[neighbors.3]
+        && !edges[neighbors.4]
+        && edges[neighbors.5];
+    v1 || v2 || v3 || v4
+}
+
+fn contains_wall_type_101x010(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 101x010
+    let v1 = edges[neighbors.0]
+        && !edges[neighbors.1]
+        && edges[neighbors.2]
+        && !edges[neighbors.3]
+        && edges[neighbors.4]
+        && !edges[neighbors.5];
+    // 010x101
+    let v2 = !edges[neighbors.0]
+        && edges[neighbors.1]
+        && !edges[neighbors.2]
+        && edges[neighbors.3]
+        && !edges[neighbors.4]
+        && edges[neighbors.5];
+    v1 || v2
+}
+
+fn contains_wall_type_101x001(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 101x001
+    let v1 = edges[neighbors.0]
+        && !edges[neighbors.1]
+        && edges[neighbors.2]
+        && !edges[neighbors.3]
+        && !edges[neighbors.4]
+        && edges[neighbors.5];
+    // 101x100
+    let v2 = edges[neighbors.0]
+        && !edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && !edges[neighbors.4]
+        && !edges[neighbors.5];
+    // 001x101
+    let v3 = !edges[neighbors.0]
+        && !edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && !edges[neighbors.4]
+        && edges[neighbors.5];
+    // 100x101
+    let v4 = edges[neighbors.0]
+        && !edges[neighbors.1]
+        && !edges[neighbors.2]
+        && edges[neighbors.3]
+        && !edges[neighbors.4]
+        && edges[neighbors.5];
+    v1 || v2 || v3 || v4
+}
+
+fn contains_wall_type_101x000(edges: &Vec<bool>, neighbors: NeighborsOneSided) -> bool {
+    // 101x000 and 000x101
+    edges[neighbors.0] && !edges[neighbors.1] && edges[neighbors.2]
+}
+
+fn contains_wall_type_011x011(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 011x011
+    let v1 = !edges[neighbors.0]
+        && edges[neighbors.1]
+        && edges[neighbors.2]
+        && !edges[neighbors.3]
+        && edges[neighbors.4]
+        && edges[neighbors.5];
+    // 110x110
+    let v2 = edges[neighbors.0]
+        && edges[neighbors.1]
+        && !edges[neighbors.2]
+        && edges[neighbors.3]
+        && edges[neighbors.4]
+        && !edges[neighbors.5];
+    v1 || v2
+}
+
+fn contains_wall_type_011x110(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 011x110
+    let v1 = !edges[neighbors.0]
+        && edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && edges[neighbors.4]
+        && !edges[neighbors.5];
+    // 110x011
+    let v2 = edges[neighbors.0]
+        && edges[neighbors.1]
+        && !edges[neighbors.2]
+        && !edges[neighbors.3]
+        && edges[neighbors.4]
+        && edges[neighbors.5];
+    v1 || v2
+}
+
+fn contains_wall_type_011x010(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 011x010
+    let v1 = !edges[neighbors.0]
+        && edges[neighbors.1]
+        && edges[neighbors.2]
+        && !edges[neighbors.3]
+        && edges[neighbors.4]
+        && !edges[neighbors.5];
+    // 110x010
+    let v2 = edges[neighbors.0]
+        && edges[neighbors.1]
+        && !edges[neighbors.2]
+        && !edges[neighbors.3]
+        && edges[neighbors.4]
+        && !edges[neighbors.5];
+    // 010x011
+    let v3 = !edges[neighbors.0]
+        && edges[neighbors.1]
+        && !edges[neighbors.2]
+        && !edges[neighbors.3]
+        && edges[neighbors.4]
+        && edges[neighbors.5];
+    // 010x110
+    let v4 = !edges[neighbors.0]
+        && edges[neighbors.1]
+        && !edges[neighbors.2]
+        && edges[neighbors.3]
+        && edges[neighbors.4]
+        && !edges[neighbors.5];
+    v1 || v2 || v3 || v4
+}
+
+fn contains_wall_type_011x001(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 011x001
+    let v1 = !edges[neighbors.0]
+        && edges[neighbors.1]
+        && edges[neighbors.2]
+        && !edges[neighbors.3]
+        && !edges[neighbors.4]
+        && edges[neighbors.5];
+    // 110x100
+    let v2 = edges[neighbors.0]
+        && edges[neighbors.1]
+        && !edges[neighbors.2]
+        && edges[neighbors.3]
+        && !edges[neighbors.4]
+        && !edges[neighbors.5];
+    // 001x011
+    let v3 = !edges[neighbors.0]
+        && !edges[neighbors.1]
+        && edges[neighbors.2]
+        && !edges[neighbors.3]
+        && edges[neighbors.4]
+        && edges[neighbors.5];
+    // 100x110
+    let v4 = edges[neighbors.0]
+        && !edges[neighbors.1]
+        && !edges[neighbors.2]
+        && edges[neighbors.3]
+        && edges[neighbors.4]
+        && !edges[neighbors.5];
+    v1 || v2 || v3 || v4
+}
+
+fn contains_wall_type_011x100(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 011x100
+    let v1 = !edges[neighbors.0]
+        && edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && !edges[neighbors.4]
+        && !edges[neighbors.5];
+    // 110x001
+    let v2 = edges[neighbors.0]
+        && edges[neighbors.1]
+        && !edges[neighbors.2]
+        && !edges[neighbors.3]
+        && !edges[neighbors.4]
+        && edges[neighbors.5];
+    // 100x011
+    let v3 = edges[neighbors.0]
+        && !edges[neighbors.1]
+        && !edges[neighbors.2]
+        && !edges[neighbors.3]
+        && edges[neighbors.4]
+        && edges[neighbors.5];
+    // 001x110
+    let v4 = !edges[neighbors.0]
+        && !edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && edges[neighbors.4]
+        && !edges[neighbors.5];
+    v1 || v2 || v3 || v4
+}
+
+fn contains_wall_type_011x000(edges: &Vec<bool>, neighbors: NeighborsOneSided) -> bool {
+    // 011x000 and 000x011
+    let v1 = !edges[neighbors.0] && edges[neighbors.1] && edges[neighbors.2];
+    // 110x000 and 000x110
+    let v2 = edges[neighbors.0] && !edges[neighbors.1] && !edges[neighbors.2];
+    v1 || v2
+}
+
+fn contains_wall_type_010x010(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 010x010
+    !edges[neighbors.0]
+        && edges[neighbors.1]
+        && !edges[neighbors.2]
+        && !edges[neighbors.3]
+        && edges[neighbors.4]
+        && !edges[neighbors.5]
+}
+
+fn contains_wall_type_010x100(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 010x100
+    let v1 = !edges[neighbors.0]
+        && edges[neighbors.1]
+        && !edges[neighbors.2]
+        && edges[neighbors.3]
+        && !edges[neighbors.4]
+        && !edges[neighbors.5];
+    // 010x001
+    let v2 = !edges[neighbors.0]
+        && edges[neighbors.1]
+        && !edges[neighbors.2]
+        && !edges[neighbors.3]
+        && !edges[neighbors.4]
+        && edges[neighbors.5];
+    // 100x010
+    let v3 = edges[neighbors.0]
+        && !edges[neighbors.1]
+        && !edges[neighbors.2]
+        && !edges[neighbors.3]
+        && edges[neighbors.4]
+        && !edges[neighbors.5];
+    // 001x010
+    let v4 = !edges[neighbors.0]
+        && !edges[neighbors.1]
+        && edges[neighbors.2]
+        && !edges[neighbors.3]
+        && edges[neighbors.4]
+        && !edges[neighbors.5];
+    v1 || v2 || v3 || v4
+}
+
+fn contains_wall_type_010x000(edges: &Vec<bool>, neighbors: NeighborsOneSided) -> bool {
+    // 010x000 and 000x010
+    !edges[neighbors.0] && edges[neighbors.1] && !edges[neighbors.2]
+}
+
+fn contains_wall_type_001x001(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 001x001
+    let v1 = !edges[neighbors.0]
+        && !edges[neighbors.1]
+        && edges[neighbors.2]
+        && !edges[neighbors.3]
+        && !edges[neighbors.4]
+        && edges[neighbors.5];
+    // 100x100
+    let v2 = edges[neighbors.0]
+        && !edges[neighbors.1]
+        && !edges[neighbors.2]
+        && edges[neighbors.3]
+        && !edges[neighbors.4]
+        && !edges[neighbors.5];
+    v1 || v2
+}
+
+fn contains_wall_type_001x100(edges: &Vec<bool>, neighbors: NeighborsTwoSided) -> bool {
+    // 001x100
+    let v1 = !edges[neighbors.0]
+        && !edges[neighbors.1]
+        && edges[neighbors.2]
+        && edges[neighbors.3]
+        && !edges[neighbors.4]
+        && !edges[neighbors.5];
+    // 100x001
+    let v2 = edges[neighbors.0]
+        && !edges[neighbors.1]
+        && !edges[neighbors.2]
+        && !edges[neighbors.3]
+        && !edges[neighbors.4]
+        && edges[neighbors.5];
+    v1 || v2
+}
+
+fn contains_wall_type_001x000(edges: &Vec<bool>, neighbors: NeighborsOneSided) -> bool {
+    // 001x000 and 000x001
+    let v1 = !edges[neighbors.0] && !edges[neighbors.1] && edges[neighbors.2];
+    // 100x000 and 000x100
+    let v2 = edges[neighbors.0] && edges[neighbors.1] && !edges[neighbors.2];
+    v1 || v2
+}
